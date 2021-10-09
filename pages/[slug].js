4e04d0164bf59566b65f4e5c33758f2de54bf74b
Baloi Radu <bradu2020@gmail.com>
2021-10-10 01:39:04 +0300

    oof

--- pages/[slug].js
+++ pages/[slug].js
@@ -11,30 +11,29 @@ export default function Post({post}) {
 
 //!tell nextjs how many pages
 export async function getStaticPaths() {
-  // const res = await fetch("http://localhost:1337/posts")
-  // const posts = await res.json()
+  const res = await fetch("http://localhost:1337/posts")
+  const posts = await res.json()
 
-  // const paths = posts.map((post) => ({
-  //   params: {slug: post.Slug}
-  // }))
+  const paths = posts.map((post) => ({
+    params: { slug: post.Slug },
+  }))
 
   return {
-    // paths,
-    paths: { params: {slug: "test"}},
+    paths,
     fallback: true, //! static vs incremental/ssr
   }
 }
 
 
 //!for each page, get data for it
-export async function getStaticProps({params}) {
-  const {slug} = params;
-  // const res = await fetch(`http://localhost:1337/posts?Slug=${slug}`)
-  // const data = await res.json()
-  // const post = data[0]
+export async function getStaticProps({ params }) {
+  const { slug } = params;
+  const res = await fetch(`http://localhost:1337/posts?Slug=${slug}`)
+  const data = await res.json()
+  const post = data[0]
 
   return {
-    props: {post}
+    props: {post},
   }
 }
 
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,8 @@ import Link from 'next/link'
 
 export default function Home({ posts }) {
   // console.log(posts)
-  const BACKEND = process.env.BACKEND
-  console.log(BACKEND)
   return (
     <div>
-    <h1>test</h1>
-    {BACKEND && <h2>{BACKEND}</h2>}
     {posts && posts.map((post) => (
       <Link href={`/${post.Slug}`} key={post.id}>
         <a>
@@ -21,12 +17,11 @@ export default function Home({ posts }) {
 
 export async function getStaticProps() {
   //get posts from api
-  // const res = await fetch("http://localhost:1337/posts")
-  // const posts = await res.json()
+  const res = await fetch("http://localhost:1337/posts")
+  const posts = await res.json()
 
-  console.log("node next server", BACKEND)
+  console.log("node next server")
   return {
-    props: {},
-    // props: {posts},
+    props: {posts},
   }
 }
